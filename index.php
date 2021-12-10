7a7846db88d7275427e591739c0690eca7b7bda4
Vozodo <raphaelbarbieri638@gmail.com>
2021-12-10 16:11:48 +0100

    feat: added first frontend

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <?php
-echo "test";
+    //Responsive navbar
+    require "imports/navbar.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" a="b">
-</head>
-<body>
-    <h1>test</h1>
-</body>
-</html>
 
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Squada</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Page content-->
+    <div class="container">
+        <div class="text-center mt-5">
+            <h1>Squada</h1>
+            <p class
+            <p class="lead">
+                Welcome to Squada!
+                Squada is an auction platform where football players who have not yet been assigned to a team are
+                auctioned by the members of the Fanta Calcio group.</p>
+        </div>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
