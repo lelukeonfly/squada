e1f8c8c397352b4fd4df58574ab1a86cbc5ae5ba
Vozodo <raphaelbarbieri638@gmail.com>
2021-12-14 11:20:30 +0100

    feat: navbarloginset (some bugs: fix tomorow)

--- imports/navbar.php
+++ imports/navbar.php
@@ -1,3 +1,4 @@
+
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0076FB;">
     <div class="container">
         <a class="navbar-brand" href="index.php">
@@ -24,10 +25,17 @@
                 </li>
             </ul>
         </div>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="login.php"><button type="button" class="btn btn-secondary btn-sm" >Login</button></a>
-            </li>
-        </ul>
+        <?php if (isset($_SESSION['user']) && !empty($_SESSION['user'])) { ?>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">Loged in</li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="logout.php">Log OUT</a></li>
+            </ul>
+            <?php } else { ?>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="login.php"><button type="button" class="btn btn-secondary btn-sm" >Login</button></a>
+                    </li>
+                </ul>
+        <?php } ?>
     </div>
 </nav>
